4bc4dcc1098a6743af04f3aebbf20eb520b83393
TPCPL <tarunpremiercorporation@gmail.com>
2018-08-10 08:55:08 +0530

    added axf

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 		var m = n.getMonth() + 1;
 		var d = n.getDate();
 	
-    	$(".os-r, .rec-r, .acc-r").each(function(){
+    	$(".os-r, .rec-r, .acc-r, .axf-s, .axf-t").each(function(){
     		$(this).bind("keyup keypressed change input", function(){
 	    		$("td span:eq(1)",this).html(po($("td input:eq(0)",this).val(),$("td input:eq(1)",this).val(),$("td input:eq(2)",this).val()))
 	    		$("td span:eq(2)",this).html(parseInt($("td span:eq(1)",this).html())*$("td input:eq(3)",this).val())
