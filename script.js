fb7a343d5d61d0c408fca82388ec71022b35604c
TPCPL <tarunpremiercorporation@gmail.com>
2018-08-25 12:39:25 +0530

    Order Alert

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
-	var po = function(qty, re, moq){
+var po = function(qty, re, moq){
 
-    	return Math.max(0,Math.ceil((re-qty)/moq)*moq)
+	return Math.max(0,Math.ceil((re-qty)/moq)*moq)
+
+}
+$(document).ready(function(){
+	var n =  new Date();
+	var y = n.getFullYear();
+	var m = n.getMonth() + 1;
+	var d = n.getDate();
     
-    }
-    $(document).ready(function(){
-		var n =  new Date();
-		var y = n.getFullYear();
-		var m = n.getMonth() + 1;
-		var d = n.getDate();
-	
-    	$(".os-r, .rec-r, .acc-r, .axf-s, .axf-t").each(function(){
-    		$(this).bind("keyup keypressed change input", function(){
-	    		$("td span:eq(1)",this).html(po($("td input:eq(0)",this).val(),$("td input:eq(1)",this).val(),$("td input:eq(2)",this).val()))
-	    		$("td span:eq(2)",this).html(parseInt($("td span:eq(1)",this).html())*$("td input:eq(3)",this).val())
-	    		$("td span:eq(3)",this).html(d + "/" + m + "/" + y);
-    		});
-    	});
+	$(".os-r, .rec-r, .acc-r, .axf-s, .axf-t").each(function(){
+		$(this).bind("keyup keypressed change input", function(){
+    		$("td span:eq(1)",this).html(po($("td input:eq(0)",this).val(),$("td input:eq(1)",this).val(),$("td input:eq(2)",this).val()))
+    		$("td span:eq(2)",this).html(parseInt($("td span:eq(1)",this).html())*$("td input:eq(3)",this).val())
+    		$("td span:eq(3)",this).html(d + "/" + m + "/" + y);
+		    var sum = 0;  
+            $(".orderquantity").each(function() {
+                
+                var val = $.trim( $(this).text() );
 
-    	$("#passcode").bind("keyup keypressed change input", function(){
-    		if($("input:eq(0)", this).val() === "navin"){
-    			$(this).css('visibility', 'hidden');
-    			$("#content").css('visibility', 'visible');
-    		};
-    	});
+                if ( val ) {
+                    val = parseFloat( val.replace( /^\$/, "" ) );
 
+                    sum += !isNaN( val ) ? val : 0;
+                }
+                if(sum >= 25.0){
+                    $(".axf-s td:nth-child(5), .axf-t td:nth-child(5)").css('background-color', 'green');    
+                } else {
+                    $(".axf-s td:nth-child(5), .axf-t td:nth-child(5)").css('background-color', 'black');    
+                }
+            }); 
+        });;        
+    });         
+
+    $("#passcode").bind("keyup keypressed change input", function(){
+		if($("input:eq(0)", this).val() === "navin"){
+			$(this).css('visibility', 'hidden');
+			$("#content").css('visibility', 'visible');
+		};
     });
+});
